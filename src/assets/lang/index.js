671ed76da7c35519298899d583007ced51a16770
lee84233 <842337932@qq.com>
2018-03-03 17:56:54 +0800

    :earth_americas: i18n

--- src/assets/lang/index.js
+++ src/assets/lang/index.js
@@ -1,8 +1,11 @@
-/**
+/*
  * 国际化
- * Author: Lee
- * Created: 2018-02-10
+ * @Author: Bruce.Lee
+ * @Date: 2018-03-02 22:24:47
+ * @Last Modified by: Bruce.Lee
+ * @Last Modified time: 2018-03-03 11:27:06
  */
+
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import store from '@/store/index'
@@ -13,8 +16,8 @@ import en from './en' // 英文
 Vue.use(VueI18n)
 
 export default new VueI18n({
-  locale: store.state.lang,
   // this.$i18n.localse = 'en' // 通过切换locale的值实现语言切换
+  locale: store.state.lang,
   messages: {
     zh,
     en
