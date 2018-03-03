3a7429a3a284b9f5bf253b0389b231e5c6cde4c4
lee84233 <842337932@qq.com>
2018-03-03 14:40:53 +0800

    :rage: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,13 @@ module.exports = {
     parser: 'babel-eslint'
   },
   env: {
-    browser: true,
+    browser: true
+  },
+  globals: {
+    console: true,
+    layer: true,
+    $: true,
+    jQuery: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
@@ -16,20 +22,32 @@ module.exports = {
     'standard'
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   // add your custom rules here
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    //禁止修改const声明的变量
-    "no-const-assign": 2,
-    //在创建对象字面量时不允许键重复 {a:1,a:1}
-    "no-dupe-keys": 2,
-    //函数参数不能重复
-    "no-dupe-args": 2
+    // 禁止修改const声明的变量
+    'no-const-assign': 2,
+    // 在创建对象字面量时不允许键重复 {a:1,a:1}
+    'no-dupe-keys': 2,
+    // 函数参数不能重复
+    'no-dupe-args': 2,
+    // 语句强制分号结尾
+    // semi: ['error', 'always'],
+    // 禁止使用console
+    'no-console': 1,
+    // 在函数定义中，括号之前，要执行一致的间距
+    // 'space-before-function-parentheses': ['error', 'never'],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'never'
+      }
+    ]
   }
 }
